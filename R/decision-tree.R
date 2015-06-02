6445aeee7b0a4f140d545682b7ac694e17d60ac8
sanpi0205 <sanpi0205@gmail.com>
2015-06-02 15:16:50 +0800

    decision tree

--- R/decision-tree.R
+++ R/decision-tree.R
@@ -1,16 +1,34 @@
-
-
-# 决策树
+# 在R中决策树代码
 set.seed(1234)
-
+data(iris)
+# 有放回抽样
 index = sample(1:2, nrow(iris), replace=T, prob=c(0.7,0.3))
 attach(iris)
 trainData = iris[index==1,]
 testData = iris[index==2,]
 
+# Conditional Inference Trees
 library('party')
-
+# 定义模型
 myFormula = Species~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
 iris_ctree = ctree(myFormula, data = trainData)
-table(predict(iris_ctree), trainData$Species)
+table(trainData$Species, predict(iris_ctree))
+# 错误率
+preMatrix = table(trainData$Species, predict(iris_ctree))
+1 - sum(diag(preMatrix)) / sum(preMatrix)
+
 print(iris_ctree)
+plot(iris_ctree)
+
+# 决策树(另一个R包)
+library('rpart')
+iris_tree = rpart(myFormula, data = trainData)
+prediction = predict(iris_tree, newdata = trainData, type = 'class')
+table(trainData$Species, prediction)
+
+# 错误率
+preMatrix = table(trainData$Species, prediction)
+1 - sum(diag(preMatrix)) / sum(preMatrix)
+
+print(iris_tree)
+plot(iris_tree)
