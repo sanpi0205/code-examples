a212ec51ba40d644946a9c4674e58cea9d1649f7
sanpi0205 <sanpi0205@gmail.com>
2016-03-02 16:55:52 +0800

    l1 penalty

--- python/sklearn/logisticRegression.py
+++ python/sklearn/logisticRegression.py
@@ -9,3 +9,34 @@ import numpy as np
 from sklearn import datasets
 
 from sklearn.linear_model import LogisticRegression
+
+import matplotlib.pyplot as plt
+
+digits = datasets.load_digits()
+
+n_test = 100
+X_train = digits.data[:-n_test]
+X_test = digits.data[-n_test:]
+
+y_train = digits.target[:-n_test]
+y_test = digits.target[-n_test:]
+
+clf = LogisticRegression(penalty = 'l2', C=0.1, random_state=100)
+
+clf.fit(X_train, y_train)
+clf.score(X_train, y_train)
+clf.score(X_test, y_test)
+
+train_score = np.array([])
+test_score = np.array([])
+
+penalty_factor = 10 ** np.arange(2, -2, -0.1)
+for i,C in enumerate(penalty_factor):
+    clf = LogisticRegression(penalty='l1', C=C, random_state=100)
+    clf.fit(X_train, y_train)
+    train_score = np.append(train_score, clf.score(X_train, y_train))
+    test_score = np.append(test_score, clf.score(X_test, y_test))
+
+scores = np.column_stack((train_score, test_score))
+# scores = np.c_[train_score， test_score]
+plt.plot(scores)
