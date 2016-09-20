74c520b26c99e812402342607608b22da90a4e20
Thomas <sanpi0205@gmail.com>
2016-09-20 10:11:59 +0800

    detect the face landmark

--- algorithm/image/face_landmark.py
+++ algorithm/image/face_landmark.py
@@ -6,3 +6,20 @@ Created on Mon Sep 19 17:09:01 2016
 """
 
 import dlib
+import os
+import dlib
+import glob
+from skimage import io
+
+predictor_path = '/home/zhangbo/software/shape_predictor_68_face_landmarks.dat'
+faces_file_path = '/home/zhangbo/Desktop/face/zw6.jpg'
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+img = io.imread(faces_file_path)
+dets = detector(img, 1)
+kk = [x for x in dets]
+shape = predictor(img, kk[0])
+
+#人脸检测点分布: http://stackoverflow.com/questions/36310147/landmarks-detection-using-dlib
